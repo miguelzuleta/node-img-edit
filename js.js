eaed8ed410a3d6e06553d7070b47a9b85a8d3882
miguelzuleta <zzuleta@hotmail.com>
2018-08-30 23:41:59 -0700

    Add files via upload

--- js.js
+++ js.js
@@ -2,23 +2,25 @@ const path = require('path')
 const fs = require('fs')
 const changeTime = require('change-file-time')
 
-const directoryPath = path.join(__dirname, 'JPG_Tintin_test');
+const directoryPath = path.join(__dirname, 'JPG_Mafalda');
 console.log(directoryPath)
 
 let fileTimer = (file, timer) => {
-	// setTimeout(() => {
-		// let newName = file
-		// 				.replace('_Image_0001.jpg', '.jpg')
-		// 				.replace('Page_', '')
-		// console.log(newName)
+	// Quino - Toda Mafalda_Page_001_Image_0001
+	setTimeout(() => {
+		let newName = file
+						.replace('Quino - Toda Mafalda_Page_', 'mafalda_')
+						.replace('_Image_0001', '')
+
 		let oldPath = `${directoryPath}/${file}`
-		let newPath = `${directoryPath}/tintin_${file}`
+		let newPath = `${directoryPath}/${newName}`
 
 		fs.rename(oldPath, newPath, err => err)
 		changeTime(newPath, err => err)
 
 		console.log(newPath)
-	// }, timer * 0)
+
+	}, timer * 61000)
 }
 
 fs.readdir(directoryPath, function (err, files) {
